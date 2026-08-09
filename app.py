cd20d17bfc9fb48f0b321efd9a6dd3703bbee5dd
flenkbm <flenkbm@gmail.com>
2026-08-09 13:57:13 +0300

    auth system

--- app.py
+++ app.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/API/helloworld")
-def hello():
-    return "Hello, World! And blahblahblah"
+@app.get("/API/login")
+def login(nickname: str, password: str):
+    with open("closedfiles/testdb.txt") as f:
+        entry = f.readline().split()
+        while entry != []:
+            if (entry[0] == nickname and entry[1] == password):
+                return int(entry[2])
+            entry = f.readline().split()
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,4 @@
 bind = "88.210.12.42:8000"
 workers = 4
 worker_class = "asgi"
+reload = True
