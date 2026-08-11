909945fb99ce447c8c6e04142bb968200fcc5249
flenkbm <flenkbm@gmail.com>
2026-08-11 15:21:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from uuid import uuid4
 
 app = FastAPI()
 
@@ -11,3 +12,24 @@ def login(nickname: str, password: str):
                 return {entry[2]}
             entry = f.readline().split()
         return -1
+
+@app.get("/API/registration")
+def registration(nickname: str, password: str):
+    with open("closedfiles/testdb.txt") as f:
+        entry = f.readline().split()
+        while entry != []:
+            if (entry[0] == nickname):
+                return -1
+            entry = f.readline().split()
+    uuid = uuid4()
+    with open("closedfiles/testdb.txt", "a") as f:
+        f.write(f"{nickname} {password} {uuid}")
+    with open(f"openfiles/{uuid}.json", "x") as f:
+        f.write(f"""{{
+    "nickname" : "{nickname}",
+    "EXP" : 0,
+    "LVL" : 0,
+    "inventory" : {{"air":2, "earth":2, "fire":2, "water":2}},
+    "created" : ["air", "earth", "fire", "water"]
+}}""")
+    return {f"{uuid}"}
