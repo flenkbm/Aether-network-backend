ee40cd4db0071a20ad5c8314f03f154cb4034f1b
flenkbm <flenkbm@gmail.com>
2026-08-15 12:26:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ def hashstr(string: str):
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
