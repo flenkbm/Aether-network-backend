e8990ada7422a381f4477e5a7b91645cc39e4e28
flenkbm <flenkbm@gmail.com>
2026-08-15 18:31:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -336,6 +336,7 @@ class getuser_data(BaseModel):
 
 @app.post("/API/admin/getuser")
 def getuser(dt: getuser_data):
+    return dt.username
     if not checkadmin(dt.password):
         return "Heeey! You're not the administrator! What are you doing here? Get away!"
     #
